5db0fc0cc0c8a5a5eca5c532a82be7cbd09b1af2
ploy <ploy_skt_at@hotmail.com>
2020-09-16 15:21:51 +0700

    QUIZ3

---
+++ js/quiz3.js
@@ -0,0 +1,14 @@
+const CalculateGrade = (scoer) => {
+    let grade = null
+    if(scoer >= 81 && scoer <= 100){
+        grade = "A"
+    } else if(scoer >= 71 && scoer <= 80){
+        grade = "B"
+    } else if(scoer >= 61 && scoer <= 70){
+        grade = "C"
+    } else if(scoer >= 51 && scoer <= 60){
+        grade = "D"
+    } else if(scoer >= 41 && scoer <= 50){
+        grade = "A"
+    } else if()
+}
