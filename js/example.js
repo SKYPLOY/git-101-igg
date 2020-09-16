9e485d6ef8c8f7848af4c593a372086eedd43e37
ploy <ploy_skt_at@hotmail.com>
2020-09-16 13:38:48 +0700

    Create example.js

---
+++ js/example.js
@@ -0,0 +1,16 @@
+
+//let name = "Ploy"
+//name = "B"
+//let test = "Hello"
+//name = `${test} B`
+
+let arr =['apple','banana','kiwi']
+console.log("arr: ",arr[1])
+let person = {
+    name:'JOKE',
+    age: 15,
+    gender: {
+        name : 'male'
+    }
+}
+console.log(person.name)
