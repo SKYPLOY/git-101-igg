bbd961f64a07716e4a4a8a928295c360bff5f6cc
ploy <ploy_skt_at@hotmail.com>
2020-09-16 15:55:23 +0700

    synchronous.js

---
+++ js/quiz4.js
@@ -0,0 +1,8 @@
+//var txt = "Hello my name is Joke";
+
+let Text = 'Hello my name is Joke'
+console.log(Text.length)
+
+var str = "Hello my name is Joke"
+var pos = str.search("Joke")
+console.log(pos)
