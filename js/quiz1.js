4f22f692e7dfbc068dabdc84dbece934ce4c6b5d
ploy <ploy_skt_at@hotmail.com>
2020-09-16 13:48:30 +0700

    quiz1

---
+++ js/quiz1.js
@@ -0,0 +1,11 @@
+let ploy = {
+    name : 'Sukitta',
+    lastname : 'Ketesaard',
+    age : '21',
+    nickname : 'Ploy',
+    hobby : 'playing game',
+    numberphone : '093-0325353'
+
+}
+   
+console.log(ploy)
