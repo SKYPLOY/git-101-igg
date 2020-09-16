ff001035c321bd97e6798a68f5f93bc62d646922
ploy <ploy_skt_at@hotmail.com>
2020-09-16 14:06:01 +0700

    [added] function file
    description of function and arrow

---
+++ js/function.js
@@ -0,0 +1,16 @@
+//function add(a,b){
+//   return a+b
+//}
+
+function mul(a,b){
+    return a*b
+}
+
+function searchCustomer(customerName){
+    //find some customer
+}
+
+const add = (a,b) => a + b
+
+console.log(add(2,3))
+console.log(mul(2,3))
